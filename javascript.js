a262cd851bec0d36c691caccf1c7cfa59219c2f5
Jonathan Lee <jonathanlee12932@gmail.com>
2024-05-20 15:02:35 -0400

    Add form submission features and fixed some scaling size issues

--- javascript.js
+++ javascript.js
@@ -2,8 +2,8 @@
 var userInfo = [];
 
 
-
-document.getElementById('userForm').addEventListener('submit', () => {
+document.getElementById('userForm').addEventListener('submit', function (event) {
+    event.preventDefault();  // Prevent the default form submission
 
     // Get the value from each input field
     let firstName = document.getElementById('firstName').value;
@@ -13,22 +13,24 @@ document.getElementById('userForm').addEventListener('submit', () => {
     let password = document.getElementById('password').value;
     let confirmPassword = document.getElementById('confirmPassword').value;
 
-    if (password != confirmPassword)
-        return;
+    // Check if passwords match
+    if (password !== confirmPassword) {
+        alert("Passwords do not match.");
+        return;  // Stop the function if passwords don't match
+    }
 
-    userInfo.push(new Account(firstName, lastName, email, phoneNumber, password))
+    // Create a new Account object and push it into the userInfo array
+    userInfo.push(new Account(firstName, lastName, email, phoneNumber, password));
+
+    console.log(userInfo);  // Optionally, log the user info to the console or handle further
 });
 
 class Account {
-
-
     constructor(firstName, lastName, email, phoneNumber, password) {
-
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
         this.phoneNumber = phoneNumber;
         this.password = password;
     }
-
 }
